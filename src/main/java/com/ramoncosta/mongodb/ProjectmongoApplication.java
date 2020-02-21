02306e720754784e2394e16117a7406628a98246
Ramon Rodrigues <ramon.rodrigues.lima@gmail.com>
2020-02-20 20:13:24 -0400

    Primeiro Commit - Projeto Criado

---
+++ src/main/java/com/ramoncosta/mongodb/ProjectmongoApplication.java
@@ -0,0 +1,13 @@
+package com.ramoncosta.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmongoApplication.class, args);
+	}
+
+}

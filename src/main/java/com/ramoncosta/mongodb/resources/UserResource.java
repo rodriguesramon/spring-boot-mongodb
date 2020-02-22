8dc5c4d15f8ba19defd48e6f0a32a4561457a32e
Ramon Rodrigues <ramon.rodrigues.lima@gmail.com>
2020-02-22 16:30:36 -0400

    Obtendo um Post por id

--- src/main/java/com/ramoncosta/mongodb/resources/UserResource.java
+++ src/main/java/com/ramoncosta/mongodb/resources/UserResource.java
@@ -1,70 +1,26 @@
 package com.ramoncosta.mongodb.resources;
 
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ramoncosta.mongodb.domain.Post;
-import com.ramoncosta.mongodb.domain.User;
-import com.ramoncosta.mongodb.dto.UserDTO;
-import com.ramoncosta.mongodb.services.UserService;
+import com.ramoncosta.mongodb.services.PostService;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/posts")
 public class UserResource {
 	
 	@Autowired
-	private UserService userService;
+	private PostService postService;
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<UserDTO>> findAll(){
-		List<User> list = (List<User>) userService.findAll();
-		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-		return ResponseEntity.ok().body(listDTO);
-	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<UserDTO> findById(@PathVariable String id){
-		User user = userService.findById(id);		
-		return ResponseEntity.ok().body(new UserDTO(user));
-	}
-	
-	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
-	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
-		User user = userService.findById(id);		
-		return ResponseEntity.ok().body(user.getPosts());
-	}
-	
-	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO){
-		User user = userService.fromDTO(userDTO);
-		user = userService.insert(user);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
-		return ResponseEntity.created(uri).build();		
-	}
-	
-	@RequestMapping(value = "/{id}",method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody UserDTO userDTO, @PathVariable String id){
-		User user = userService.fromDTO(userDTO);
-		user.setId(id);
-		user = userService.update(user);
-		return ResponseEntity.noContent().build();		
-	}
-	
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable String id){
-		userService.delete(id);		
-		return ResponseEntity.noContent().build();
-	}
-	
-		
+	public ResponseEntity<Post> findById(@PathVariable String id){
+		Post post = postService.findById(id);		
+		return ResponseEntity.ok().body(post);
+	}		
 }
---
+++ src/main/java/com/ramoncosta/mongodb/services/PostService.java
@@ -0,0 +1,28 @@
+package com.ramoncosta.mongodb.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ramoncosta.mongodb.domain.Post;
+import com.ramoncosta.mongodb.repository.PostRepository;
+import com.ramoncosta.mongodb.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Post post = postRepository.findById(id).orElse(null);
+		if(post == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return post;
+	}
+	
+	
+	
+	
+	
+}

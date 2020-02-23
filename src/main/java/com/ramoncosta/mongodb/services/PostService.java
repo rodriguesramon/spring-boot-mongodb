a3441c326f4fb2d11db4b0f6907506086c718402
Ramon Rodrigues <ramon.rodrigues.lima@gmail.com>
2020-02-23 13:42:57 -0400

    Consulta com Vários Critérios

--- src/main/java/com/ramoncosta/mongodb/services/PostService.java
+++ src/main/java/com/ramoncosta/mongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.ramoncosta.mongodb.services;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,8 +28,8 @@ public class PostService {
 		return postRepository.findByTitle(text);
 	}
 	
-	
-	
-	
-	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){		
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return postRepository.fullSearch(text, minDate, maxDate);
+	}
 }
